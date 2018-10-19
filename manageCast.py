b50240e4d7b7e2266bacc87cf1679fa15448fd32
Ladislav Lipka <lipka28@gmail.com>
2018-10-19 16:46:34 +0200

    Added Media controll

--- manageCast.py
+++ manageCast.py
@@ -1,4 +1,5 @@
 import pychromecast
+import time
 
 
 class castDevice:
@@ -6,6 +7,8 @@ class castDevice:
     def __init__(self):
         self.available_devices = None
         self.chosen_device = None
+        self.cast = None
+        self.cast_media = None
 
     def find_devices(self):
         self.available_devices = pychromecast.get_chromecasts()
@@ -17,3 +20,22 @@ class castDevice:
             names.append(cc.device.friendly_name)
 
         return names
+
+    def choose_casting_device(self):
+        self.cast = next(cc for cc in self.available_devices if cc.device.friendly_name == self.chosen_device)
+        self.cast.wait()
+        self.cast_media = self.cast.media_controller
+        print(self.cast.device)
+
+    def play_media(self):
+
+        self.cast_media.play_media('http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4', 'video/mp4')
+
+    def stop_casting(self):
+        self.cast_media.play_media('','')
+
+    def pause(self):
+        self.cast_media.pause()
+
+    def resume(self):
+        self.cast_media.play()
--- youCast.py
+++ youCast.py
@@ -10,38 +10,52 @@ class App:
 
         frame = Frame(master)
         frame.master.title("YouCast")
-        select_frame = Frame(frame)
-        frame['height'] = 600
-        frame['width'] = 800
-        frame.pack_propagate(0)
         frame.pack()
+
+        select_frame = Frame(frame)
         select_frame.pack(side=TOP)
 
-        self.cast_manager = manageCast.castDevice()
+        media_buttons = Frame(frame)
 
+        self.cast_manager = manageCast.castDevice()
         self.device_selector = Listbox(select_frame)
         self.device_selector.pack(side=LEFT)
 
-        self.search_button = Button(
-            select_frame, text="Search for cast devices", command=self.list_cast_devices
-            )
+        self.search_button = Button(select_frame, text="Search for cast devices", command=self.list_cast_devices)
         self.search_button.pack(side=LEFT)
 
-        self.hi_there = Button(select_frame, text="Hello", command=self.say_hi)
-        self.hi_there.pack(side=LEFT)
+        self.btn_select = Button(select_frame, text="Select Device", command=self.choose_device)
+        self.btn_select.pack(side=LEFT)
 
-    def say_hi(self):
-        print ("hi there, everyone!")
+        self.txt_url = Entry(master)
+        self.txt_url.insert(0, "Url goes here")
+        self.txt_url.pack(side=TOP)
+
+        media_buttons.pack(side=TOP)
+        self.btn_play = Button(media_buttons, text="Play new video", command=self.play_url)
+        self.btn_play.pack(side=LEFT)
+
+        self.btn_stop = Button(media_buttons, text="Stop", command=self.cast_manager.stop_casting)
+        self.btn_stop.pack(side=LEFT)
+
+        self.btn_stop = Button(media_buttons, text="Pause", command=self.cast_manager.pause)
+        self.btn_stop.pack(side=LEFT)
+
+        self.btn_stop = Button(media_buttons, text="Resume", command=self.cast_manager.resume)
+        self.btn_stop.pack(side=LEFT)
+
+    def choose_device(self):
+        self.cast_manager.chosen_device = self.device_selector.selection_get()
+        self.cast_manager.choose_casting_device()
 
     def list_cast_devices(self):
         self.search_button['state'] = DISABLED
+        self.search_button['text'] = "Searching..."
         self.device_selector.delete(0, END)
         workerThred = threading.Thread(target=self.cast_manager.find_devices, args=(), kwargs={})
         workerThred.start()
         checkingThread = threading.Thread(target=self.list_results_when_ready, args=([workerThred]), kwargs={})
         checkingThread.start()
-
-
 
     def list_results_when_ready(self, child):
         child.join()
@@ -49,7 +63,14 @@ class App:
         for device in deviceList:
             self.device_selector.insert(END, device)
 
+        self.search_button['text'] = "Search for cast devices"
         self.search_button['state'] = ACTIVE
+
+    def play_url(self):
+        self.cast_manager.play_media()
+
+
+
 
 
 root = Tk()
